94db59844087c0188210c0545ae6ec2715d238d7
elamannaMeli <elias.lamanna@mercadolibre.com>
2021-05-27 20:31:25 -0300

    ejercicio 4

---
+++ modulo 5/clase 2/practica TM/src/main/java/ejercicio4/Fraccion.java
@@ -0,0 +1,56 @@
+package ejercicio4;
+
+public class Fraccion {
+
+    //int-int
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public float multiplicar(int a, int b) {
+        return a*b;
+    }
+
+    public float dividir(int a, int b) {
+        return a/b;
+    }
+
+    //float-int
+    public float suma(float a, int b) {
+        return a + b;
+    }
+
+    public float restar(float a, int b) {
+        return a - b;
+    }
+
+    public float multiplicar(float a, int b) {
+        return a*b;
+    }
+
+    public float dividir(float a, int b) {
+        return a/b;
+    }
+
+    //int-float
+    public float suma(int a, float b) {
+        return a + b;
+    }
+
+    public float restar(int a, float b) {
+        return a - b;
+    }
+
+    public float multiplicar(int a, float b) {
+        return a*b;
+    }
+
+    public float dividir(int a, float b) {
+        return a/b;
+    }
+
+}
